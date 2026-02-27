9137520770699f1356a780c35e008178591e60d0
MohammadAbuAwadd <147884198+abuawadd@users.noreply.github.com>
2026-02-27 22:31:26 +0300

    Update heatmap functionality and remove deprecated files

--- src/app.py
+++ src/app.py
@@ -1,26 +1,32 @@
-import uuid
 from flask import Flask
 import dash
-from dash import Dash, dcc, html, callback, Input, Output, no_update, State, callback_context
+from dash import (
+    callback,
+    Input,
+    Output,
+    no_update,
+    State,
+    callback_context,
+)
 import dash_cytoscape as cyto
 import dash_bootstrap_components as dbc
 from flask_caching import Cache
-cyto.load_extra_layouts()
-
 from components.navbar import get_navbar
+
+cyto.load_extra_layouts()
 
 server = Flask(__name__)
 
 # Local-friendly cache (persists between runs). For Redis, swap config.
 config = {
-    "DEBUG": True,          # some Flask specific configs
+    "DEBUG": True,  # some Flask specific configs
     "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
-    "CACHE_DEFAULT_TIMEOUT": 3600
+    "CACHE_DEFAULT_TIMEOUT": 3600,
 }
 # tell Flask to use the above defined config
 server.config.from_mapping(config)
 cache = Cache(server)
-server.config['SERVER_CACHE']=cache
+server.config["SERVER_CACHE"] = cache
 
 app = dash.Dash(
     __name__,
@@ -28,7 +34,7 @@ app = dash.Dash(
     suppress_callback_exceptions=True,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     use_pages=True,
-    url_base_pathname="/"
+    url_base_pathname="/",
 )
 
 # Responsible to make a desktop-app like structure
@@ -37,7 +43,7 @@ APP_STYLE = {
     "display": "flex",
     "flexDirection": "column",
     "padding": "0",
-    "backgroundColor": "#f8f9fa"
+    "backgroundColor": "#f8f9fa",
 }
 
 navbar = get_navbar(dash.page_registry.values())
@@ -48,34 +54,9 @@ app.layout = dbc.Container(
     style=APP_STYLE,
     children=[
         navbar,
-        dbc.Container(
-            dash.page_container,
-            fluid=True,
-            className="dash-page-container"
-        )
+        dbc.Container(dash.page_container, fluid=True, className="dash-page-container"),
     ],
 )
-
-
-# @callback(
-#     Output("navbar-content", "className"),
-#     Input("navbar-title", "n_clicks"),
-#     State("navbar-content", "className"),
-#     prevent_initial_call=True    
-# )
-# def toggle_navbar_dropdowns(title ,current_class):
-    
-#     if title:
-#         if current_class and "d-none" in current_class:
-#             # Show the dropdowns
-#             return "ms-auto"
-#         else:
-#             # Hide the dropdowns
-#             return "ms-auto d-none"
-
-    
-#     return no_update
-
 
 
 @callback(
@@ -83,31 +64,33 @@ app.layout = dbc.Container(
     Input("navbar-title", "n_clicks"),
     Input("navbar-title-sm", "n_clicks"),
     State("navbar", "children"),
-    prevent_initial_call=True    
+    prevent_initial_call=True,
 )
 def toggle_navbar_dropdowns(title, title_sm, children):
     ctx = callback_context
-    
+
     if not ctx.triggered:
         return no_update
-    
-    triggered_id = ctx.triggered[0]['prop_id'].split('.')[0]
-    
-    if triggered_id == 'navbar-title':
-        children[0]['props']['className'] = children[0]['props']['className'].replace("d-none", "")
+
+    triggered_id = ctx.triggered[0]["prop_id"].split(".")[0]
+
+    if triggered_id == "navbar-title":
+        children[0]["props"]["className"] = children[0]["props"]["className"].replace(
+            "d-none", ""
+        )
         for e in children[1:]:
-            e['props']['className'] = e['props']['className'] + " d-none"
+            e["props"]["className"] = e["props"]["className"] + " d-none"
         return children
-    elif triggered_id == 'navbar-title-sm':
-        children[0]['props']['className'] = children[0]['props']['className'] + " d-none"
+    elif triggered_id == "navbar-title-sm":
+        children[0]["props"]["className"] = (
+            children[0]["props"]["className"] + " d-none"
+        )
         for e in children[1:]:
-            e['props']['className'] = e['props']['className'].replace("d-none", "")
+            e["props"]["className"] = e["props"]["className"].replace("d-none", "")
         return children
-    
+
     return no_update
 
 
-
-
-if __name__ == '__main__':
-    app.run(debug=True,port=8050)
+if __name__ == "__main__":
+    app.run(debug=True, port=8050)
--- src/components/navbar.py
+++ src/components/navbar.py
@@ -1,6 +1,6 @@
-import dash
-from dash import Dash, dcc, html
+from dash import html
 import dash_bootstrap_components as dbc
+
 
 def get_navbar(page_registry):
     """page_registry = dash.page_registry.values()"""
@@ -10,33 +10,44 @@ def get_navbar(page_registry):
         path = page["path"]
         pc = path.split("/")[1:]
         if len(pc) > 1:
-            parent,child = pc
+            parent, child = pc
             if parent not in dropdowns:
                 dropdowns[parent] = []
             dropdowns[parent].append(child)
 
-
-    ddowns = [    
+    ddowns = [
         dbc.DropdownMenu(
             label=parent.capitalize(),
-            children=[
-                dbc.DropdownMenuItem(
-                    c,
-                    href=f"/{parent}/{c}"
-                ) for c in child
-            ],
+            children=[dbc.DropdownMenuItem(c, href=f"/{parent}/{c}") for c in child],
             nav=True,
             in_navbar=True,
-            className="text-white"
-        ) for parent, child in dropdowns.items()
+            className="text-white",
+        )
+        for parent, child in dropdowns.items()
     ]
 
     return dbc.Navbar(
         children=[
-            html.A("N", href="#",className="text-white text-decoration-none m-2 d-none",id="navbar-title-sm",n_clicks=0),
-            html.A("PyCoregnet", href="#", className="text-white text-decoration-none m-2",id="navbar-title",n_clicks=0),
+            html.A(
+                "PyCoregnet",
+                href="#",
+                className="text-white text-decoration-none m-2 d-none",
+                id="navbar-title-sm",
+                n_clicks=0,
+            ),
+            html.A(
+                "PyCoregnet",
+                href="#",
+                className="text-white text-decoration-none m-2",
+                id="navbar-title",
+                n_clicks=0,
+            ),
             dbc.Nav(
-                children=[dbc.NavLink("Home", href="/")] + ddowns,
+                children=[
+                    dbc.NavLink("Home", href="/"),
+                ]
+                + ddowns
+                + [dbc.NavLink("Heatmap", href="/heatmap")],  # TODO Update
                 navbar=True,
                 id="navbar-content",
                 className="",
@@ -44,60 +55,6 @@ def get_navbar(page_registry):
         ],
         color="dark",
         dark=True,
-        className="px-0 w-10 position-absolute z-3 shadow rounded-pill m-2 px-3",
-        id="navbar"
+        className="w-10 position-absolute z-3 shadow rounded-end-pill rounded-start-0 mt-2 px-2",
+        id="navbar",
     )
-
-
-"""
-@app.callback(
-    Output("navbar","children"),
-    Input("navbar-title","n_clicks"),
-    State("navbar","children"),
-    
-)
-def navbar_collapse(n_clicks,children):
-    for e in children:
-        if 'props' in e and 'className' in e['props']:
-            e['props']['className'] = e['props']['className'] + ' d-none'
-    return children
-    
-    return no_update
-
-@callback(
-    Output("navbar-dropdowns", "className"),
-    Input("navbar-button", "n_clicks"),
-    State("navbar-dropdowns", "className"),
-)
-def toggle_navbar_dropdowns(n_clicks, current_class):
-    if not n_clicks:
-        return "ms-auto"
-    
-    # Toggle between showing and hiding with no animation
-    if current_class and "d-none" in current_class:
-        # Show the dropdowns
-        return "ms-auto"
-    else:
-        # Hide the dropdowns
-        return "ms-auto d-none"
-
-@callback(
-    Output("navbar-dropdowns", "style"),
-    Input("navbar-button", "n_clicks"),
-    State("navbar-dropdowns", "style"),
-)
-def toggle_navbar_dropdowns(n_clicks, current_style):
-    if not n_clicks:
-        return {}
-    
-    # Toggle display between 'none' and default
-    if current_style is None:
-        current_style = {}
-    
-    if current_style.get("display") == "none":
-        # Show the dropdowns
-        return {**current_style, "display": "flex"}
-    else:
-        # Hide the dropdowns
-        return {**current_style, "display": "none"}
-"""
